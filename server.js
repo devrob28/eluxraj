b5cedb8b4080b484337c99e3d70614e4c785fdf8
Robert Melendez <rm28@Roberts-MacBook-Pro.local>
2025-11-26 18:08:10 -0800

    Add body parser middleware

--- server.js
+++ server.js
@@ -34,19 +34,9 @@ app.use(helmet({
 
 app.use(cors());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
